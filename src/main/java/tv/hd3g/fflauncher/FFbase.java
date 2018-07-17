05613951e50e5eed0715e50465c89e4e023ea571
hdsdi3g <admin@hd3g.tv>
2018-07-17 22:55:21 +0200

    Init ffmpeg dev side

--- src/main/java/tv/hd3g/fflauncher/FFbase.java
+++ src/main/java/tv/hd3g/fflauncher/FFbase.java
@@ -22,4 +22,41 @@ import org.apache.logging.log4j.Logger;
 public class FFbase {
 	private static final Logger log = LogManager.getLogger();
 	// TODO implements ffbase
+	
+	/*
+	TODO parse version
+	WINDOWS:
+	
+	ffmpeg -version
+	
+	ffmpeg version 4.0 Copyright (c) 2000-2018 the FFmpeg developers
+	built with gcc 7.3.0 (GCC)
+	configuration: --enable-gpl --enable-version3 --enable-sdl2 --enable-bzlib --enable-fontconfig --enable-gnutls --enable-iconv --enable-libass --enable-libbluray --enable-libfreetype --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenjpeg --enable-libopus --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libtheora --enable-libtwolame --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxml2 --enable-libzimg --enable-lzma --enable-zlib --enable-gmp --enable-libvidstab --enable-libvorbis --enable-libvo-amrwbenc --enable-libmysofa --enable-libspeex --enable-libxvid --enable-libaom --enable-libmfx --enable-amf --enable-ffnvcodec --enable-cuvid --enable-d3d11va --enable-nvenc --enable-nvdec --enable-dxva2 --enable-avisynth
+	libavutil      56. 14.100 / 56. 14.100
+	libavcodec     58. 18.100 / 58. 18.100
+	libavformat    58. 12.100 / 58. 12.100
+	libavdevice    58.  3.100 / 58.  3.100
+	libavfilter     7. 16.100 /  7. 16.100
+	libswscale      5.  1.100 /  5.  1.100
+	libswresample   3.  1.100 /  3.  1.100
+	libpostproc    55.  1.100 / 55.  1.100
+	
+	Linux:
+	
+	ffmpeg -version
+	
+	ffmpeg version 4.0 Copyright (c) 2000-2018 the FFmpeg developers
+	built with gcc 6.3.0 (Debian 6.3.0-18+deb9u1) 20170516
+	configuration: --as=yasm --enable-gpl --enable-version3 --enable-nonfree --enable-libmp3lame --enable-libbluray --enable-libtheora --enable-libvorbis --enable-libtwolame --enable-libvpx --enable-libxvid --enable-libopus --enable-librtmp --enable-libspeex --enable-libwavpack --enable-libwebp --enable-libzvbi --enable-libx264 --enable-libx265 --enable-libfontconfig --enable-libfreetype --enable-libopenjpeg --enable-librsvg --enable-libxml2 --enable-libzmq --enable-openssl --enable-libaom --enable-libkvazaar --enable-libopenh264 --enable-libvidstab
+	libavutil      56. 14.100 / 56. 14.100
+	libavcodec     58. 18.100 / 58. 18.100
+	libavformat    58. 12.100 / 58. 12.100
+	libavdevice    58.  3.100 / 58.  3.100
+	libavfilter     7. 16.100 /  7. 16.100
+	libswscale      5.  1.100 /  5.  1.100
+	libswresample   3.  1.100 /  3.  1.100
+	libpostproc    55.  1.100 / 55.  1.100
+	
+	
+	*/
 }
